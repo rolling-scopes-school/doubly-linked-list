8e3d2ac465df69058f67400ccbd1ad2618a0083f
Kate Minich <kate@openledger.info>
2017-01-27 19:59:24 +0300

    my-list: feature2

--- src/node.js
+++ src/node.js
@@ -1,14 +1,7 @@
-class Node {
-    constructor(data = null, prev = null, next = null) {
-        this.data = data;
-        this.prev = prev;
-        this.next = next;
-    }
-}
+
 function La () {
     return 21;
 }
 function La2 () {
     return 21;
 }
-module.exports = Node;
