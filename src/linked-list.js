b257edd17c364e26062a37898c2ae7fe7211efac
Robert Grigoryan <dev.robert.grigoryan@gmail.com>
2017-01-27 19:00:50 +0300

    I am stuck

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,80 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() { 
 
-    append(data) {}
+        this.data = []; 
+        this.length = 0 
 
-    head() {}
+    }
 
-    tail() {}
+    append(data) {
 
-    at(index) {}
+        this.data[this.data.length] = data
 
-    insertAt(index, data) {}
+    }
 
-    isEmpty() {}
+    head() { 
 
-    clear() {}
+        return this.data[0]
 
-    deleteAt(index) {}
+    }
 
-    reverse() {}
+    tail() { 
 
-    indexOf(data) {}
+        return this.data[this.data.length-1]
+
+    }
+
+    at(index) { 
+
+        return this.data[index]
+
+    }
+
+    insertAt(index, data) { 
+
+        this.data[index] = data
+
+    }
+
+    isEmpty() { 
+
+        return this.data.length == 0
+
+    }
+
+    clear() { 
+
+        for (var i = this.data.length - 1; i >= 0; i--) {
+            this.data[i] = null
+        }
+
+    }
+
+    deleteAt(index) { 
+
+        this.data.splice(this.data.indexOf(index), 1)
+
+    }
+
+    reverse() {
+
+        this.data.reverse()
+
+    }
+
+    indexOf(data) {
+
+        for (var i = this.data.length - 1; i >= 0; i--) {
+            if (this.data[i] == data) {
+                return i
+            } else {
+                return -1
+            }
+        }
+
+    }
 }
 
 module.exports = LinkedList;
