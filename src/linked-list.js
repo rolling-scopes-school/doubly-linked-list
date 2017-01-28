b14cd0fbfc9e25e73b14ae561f57ac4048d92699
LeonidH <leanid.hubar@gmail.com>
2017-01-28 16:46:42 +0300

    Revert "doubly-linked-list/feature: implement Append method"
    This reverts commit 341c3e9780bf52605e17acc2cecdc017a937732b.

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,47 +1,19 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {
-        this._head=null;
-        this._tail=null;
-        this.length=0;        
-    }
-    /*Append new node to the LinkedList 
-     * If LinkedList is empty head and tail are new node.
-     * Or if LinkedList is not empty then set node.prev as current tail
-     * and set new tail as new node.
-     */
-    append(data) {
-        var node = new Node(data);
-        if (this.isEmpty()) {
-            this._head = node;
-        }else
-        {
-            node.prev = this._tail;
-            this._tail.next = node;
-        }
-        this._tail = node;
-        this.length++;
-    }
+    constructor() {}
 
-    head() {
-        return this._head.data;
-    }
+    append(data) {}
 
-    tail() {
-        return this._tail.data;
-    }
+    head() {}
+
+    tail() {}
 
     at(index) {}
 
     insertAt(index, data) {}
-    /*Returns true if LinkedList is empty otherwise returns false*/
-    isEmpty() {
-        if (this._head==null) {
-            return true;
-        }
-        return false;
-    }
+
+    isEmpty() {}
 
     clear() {}
 
