341c3e9780bf52605e17acc2cecdc017a937732b
LeonidH <leanid.hubar@gmail.com>
2017-01-28 14:33:44 +0300

    doubly-linked-list/feature: implement Append method
    Were implemented "append", "head", "tail" and "isEmpty"

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,19 +1,47 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head=null;
+        this._tail=null;
+        this.length=0;        
+    }
+    /*Append new node to the LinkedList 
+     * If LinkedList is empty head and tail are new node.
+     * Or if LinkedList is not empty then set node.prev as current tail
+     * and set new tail as new node.
+     */
+    append(data) {
+        var node = new Node(data);
+        if (this.isEmpty()) {
+            this._head = node;
+        }else
+        {
+            node.prev = this._tail;
+            this._tail.next = node;
+        }
+        this._tail = node;
+        this.length++;
+    }
 
-    append(data) {}
+    head() {
+        return this._head.data;
+    }
 
-    head() {}
-
-    tail() {}
+    tail() {
+        return this._tail.data;
+    }
 
     at(index) {}
 
     insertAt(index, data) {}
-
-    isEmpty() {}
+    /*Returns true if LinkedList is empty otherwise returns false*/
+    isEmpty() {
+        if (this._head==null) {
+            return true;
+        }
+        return false;
+    }
 
     clear() {}
 
