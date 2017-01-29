5388a50a23f5eb3693e40fbc69e111a28f11436c
egorhormash <marmelad@MacBook-Pro-Egor.local>
2017-01-30 00:41:35 +0300

    doubly-linked-list: added some methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,42 +2,93 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
-        this.lenght = 0;
-        this.head = null;
-        this.tail = null;
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
     }
 
-    append(data) {}
+    append(data) {
+        var node = new Node(data);
+        if (this.length == 0) {
+            this._head = node;
+            this._tail = node;
+        } else {
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;
+        }
+        this.length++;
+        return this;
+    }
 
     head() {
-        if (this.lenght == 0) {
+        if (this.length == 0) {
             return null;
-        } 
-        return this.head.data;
+        } else {
+            return this._head.data;
+        }
     }
 
     tail() {
-        if (this.lenght == 0) {
+        if (this.length == 0) {
             return null;
+        } else {
+        return this._tail.data;
         }
-        return this.tail.data;
     }
 
-    at(index) {}
+    at(index) {
+        if (index <= this.length && index >= 0) {
+            var node = this._head;
+            var i = 0;
+            while (i != index) {
+                node = node.next;
+                i++;
+            } 
+        } return node.data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        if (index <= this.length-1 && index >= 0) {
+            var node = this._head;
+            var i = 0;
+            while (i != index) {
+                node = node.next;
+                i++;
+            }  node.data = data;
+        } return this;
+    }
 
     isEmpty() {
-        return this.lenght === 0;
+        if (this.length == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    clear() {}
+
+    clear() {
+        this.length == 0;
+        return this;
+    }
 
     deleteAt(index) {}
 
     reverse() {}
 
-    indexOf(data) {}
+    indexOf(data) {
+        var index = 0;
+        var node = this._head;
+        while (this.length != index) {
+            if (node.data == data) {
+                return index;
+            }
+            node = node.next;
+            index++;
+        }
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
